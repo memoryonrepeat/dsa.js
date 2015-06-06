68ee344ba8b74ca0866d6f47a11600d095af550e
Thanh Nguyen <tienthanh8490@gmail.com>
2015-06-06 11:09:31 +0800

    Add dependencies and gitignore and starter modules

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  LinkedList: require('./lib/linked-list'),
+  PriorityQueue: require('./lib/priority-queue'),
+  BinarySearchTree: require('./lib/binary-search-tree')
+  version: require('./lib/version')
+};
