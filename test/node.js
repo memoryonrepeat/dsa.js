770053db18f15c169c18df4bab20b04b6bdc06cc
Thanh Nguyen <tienthanh8490@gmail.com>
2015-06-17 12:16:52 +0800

    addLast() and test and stricter assertion

--- test/node.js
+++ test/node.js
@@ -8,35 +8,35 @@ describe('Node', function(){
 
 	describe('#getValue()', function(){
 		it('should return current value of the node', function(){
-			assert.equal(1, node.getValue(), 'Current value is 1');
+			assert.strictEqual(1, node.getValue(), 'Current value is 1');
 		});
 	});
 
 	describe('#hasNext()', function(){
 		it('should return whether the node has next value', function(){
-			assert.equal(false, node.hasNext(), 'node has no next value');
+			assert.strictEqual(false, node.hasNext(), 'node has no next value');
 			node.setNext(2);
-			assert.equal(true, node.hasNext(), 'node has next value');
+			assert.strictEqual(true, node.hasNext(), 'node has next value');
 		});
 	});
 
 	describe('#getNext()', function(){
 		it('should return current next value of the node', function(){
-			assert.equal(2, node.getNext(), 'Current next value is 2');
+			assert.strictEqual(2, node.getNext(), 'Current next value is 2');
 		});
 	});
 
 	describe('#setValue(value)', function(){
 		it('should set value of the node to the indicated one', function(){
 			node.setValue(3);
-			assert.equal(3, node.getValue(), 'Current value is 3');
+			assert.strictEqual(3, node.getValue(), 'Current value is 3');
 		});
 	});	
 
 	describe('#setNext(value', function(){
 		it('should set next value of the node to the indicated one', function(){
 			node.setNext(4);
-			assert.equal(4, node.getNext(), 'Current next value is 4');
+			assert.strictEqual(4, node.getNext(), 'Current next value is 4');
 		});
 	});
 
--- test/singly-linked-list.js
+++ test/singly-linked-list.js
@@ -9,40 +9,63 @@ describe('SinglyLinkedList', function(){
 
 	describe('#isEmpty()', function(){
 		it('should tell whether the list is empty', function(){
-			assert.equal(true, list.isEmpty(), 'Current list is empty');
+			assert.strictEqual(true, list.isEmpty(), 'Current list is empty');
 		});
 	});
 
 	describe('#addFirst()', function(){
 		it('should add the item to list', function(){
 			list.addFirst(5);
-			assert.equal(5, list.getFirst().getValue(), 'Value of first item is 5');
-			assert.equal(1, list.getSize(), 'List has size 1');
+			assert.strictEqual(5, list.getFirst().getValue(), 'Value of first item is 5');
+			assert.strictEqual(1, list.getSize(), 'List has size 1');
 		});
 	});
 
 	describe('#getFirst()', function(){
 		it('should get the first item of the list', function(){
 			list.addFirst(6);
-			assert.equal(6, list.getFirst().getValue(), 'Value of first item is 6');
+			assert.strictEqual(6, list.getFirst().getValue(), 'Value of first item is 6');
 		});
 	});	
 
 	describe('#getSize()', function(){
 		it('should return the size of the list', function(){
 			list.addFirst(7);
-			assert.equal(3, list.getSize(), 'List has size 3');
+			assert.strictEqual(3, list.getSize(), 'List has size 3');
+			list.addFirst(8);
+			assert.strictEqual(4, list.getSize(), 'List has size 4');
 		});
 	});	
 
 	describe('#removeFirst()', function(){
 		it('should remove first element of the list', function(){
-			assert.equal(7, list.getFirst().getValue(), 'Value of first item is 7');
+			assert.deepEqual([8,7,6,5], list.toArray(), 'List equals to [8,7,6,5]');
 			list.removeFirst();
-			assert.equal(2, list.getSize(), 'List has size 2');
-			assert.equal(6, list.getFirst().getValue(), 'Value of first item is 6');
-			assert.equal(5, list.getFirst().getNext().getValue(), 'Value of second item is 5');
+			assert.deepEqual([7,6,5], list.toArray(), 'List equals to [7,6,5]');
+		});
+	});
+
+	describe('#addLast()', function(){
+		it('should add the item to the end of the list', function(){
+			assert.deepEqual([7,6,5], list.toArray(), 'List equals to [7,6,5]');
+			list.addLast(4);
+			assert.deepEqual([7,6,5,4], list.toArray(), 'List equals to [7,6,5,4]');
 		});
 	});
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
