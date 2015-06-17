606d1f0b0827e08e5d0da9a8786263a631362322
Thanh Nguyen <tienthanh8490@gmail.com>
2015-06-17 10:43:47 +0800

    More test for singly linked list

--- test/singly-linked-list.js
+++ test/singly-linked-list.js
@@ -13,12 +13,25 @@ describe('SinglyLinkedList', function(){
 		});
 	});
 
-
-
 	describe('#addFirst()', function(){
 		it('should add the item to list', function(){
 			list.addFirst(5);
 			assert.equal(5, list.getFirst().getValue(), 'Value of first item is 5');
+			assert.equal(1, list.getSize(), 'List has size 1');
+		});
+	});
+
+	describe('#getFirst()', function(){
+		it('should get the first item of the list', function(){
+			list.addFirst(6);
+			assert.equal(6, list.getFirst().getValue(), 'Value of first item is 6');
+		});
+	});	
+
+	describe('#getSize()', function(){
+		it('should return the size of the list', function(){
+			list.addFirst(7);
+			assert.equal(3, list.getSize(), 'List has size 3');
 		});
 	});	
 
